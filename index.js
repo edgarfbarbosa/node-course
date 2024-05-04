0f0c96f3b91d284abe78c07ea2a71f9294a114bc
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-05-04 17:00:43 -0300

    feat: adiciona leitura dos arquivos html e substituição de seus marcadores

--- index.js
+++ index.js
@@ -30,6 +30,35 @@ const url = require('url')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 // Converte a string JSON em um objeto JavaScript.
 const dataObj = JSON.parse(data)
+// Lê o conteúdo dos arquivos HTML de forma síncrona e atribui a variáveis.
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
+/**
+* Substitui os marcadores de posição nos templates pelos dados do produto.
+* @param {string} template - O template HTML a ser processado.
+* @param {Object} product - O objeto contendo os dados do produto.
+* @returns {string} - Template HTML com os marcadores de posição substituídos pelos dados do produto.
+*/
+const replaceTemplate = (template, product) => {
+  // Substitui {%PRODUCTNAME%} pelo nome do produto no template.
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+  // Substitui {%IMAGE%} pela URL da imagem do produto no template.
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+  // Se o produto não for orgânico, adiciona a classe 'not-organic' para estilização.
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+  }
+  // Retorna o template HTML com todos os marcadores de posição substituídos pelos dados do produto.
+  return output
+}
 
 /**
 * Cria um servidor HTTP.
@@ -40,8 +69,19 @@ const server = http.createServer((req, res) => {
   const pathName = req.url // Atribui o caminho da URL da solicitação HTTP para uma constante.
   
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is OVERVIEW')
+    // Overview page
+    // Status e o tipo de conteúdo da resposta
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    // Gera o HTML dos cards dos produtos substituindo os marcadores nos templates.
+    const cardsHtml = dataObj.map(element => replaceTemplate(tempCard, element)).join('')
+    // Substitui o marcador de posição {%PRODUCT_CARDS%} no template de visão geral.
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    // Retorna a resposta com o HTML da página de visão geral.
+    res.end(output)
   } else if (pathName === '/product') {
+    // Product page
     res.end('This is the PRODUCT')
   } else if (pathName === '/api') {
     /**
@@ -49,11 +89,13 @@ const server = http.createServer((req, res) => {
     * @param {number} 200 - Código de status HTTP 200 (OK).
     * @param {Object} {'Content-Type': 'application/json'} - O header de resposta com o tipo de conteúdo.
     */
+    //  API page
     res.writeHead(200, {
       'Content-Type': 'application/json'
     })
     res.end(data)
   } else {
+    // Not found page
     res.writeHead(404, {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world'

d8cd0f8082fd965082a261b160a97bec7e1242ec
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-05-06 17:21:56 -0300

    feat: implementa requisição da função replaceTemplate

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const http = require('http')
-const path = require('path')
 const url = require('url')
+
+const replaceTemplate = require('./modules/replaceTemplate')
 
 /**
 * Lê o conteúdo de um arquivo de texto de forma síncrona e escreve em outro arquivo de texto de forma síncrona.
@@ -35,31 +36,6 @@ const dataObj = JSON.parse(data)
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
-
-/**
-* Substitui os marcadores de posição nos templates pelos dados do produto.
-* @param {string} template - O template HTML a ser processado.
-* @param {Object} product - O objeto contendo os dados do produto.
-* @returns {string} - Template HTML com os marcadores de posição substituídos pelos dados do produto.
-*/
-const replaceTemplate = (template, product) => {
-  // Substitui {%PRODUCTNAME%} pelo nome do produto no template.
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
-  // Substitui {%IMAGE%} pela URL da imagem do produto no template.
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
-  // Se o produto não for orgânico, adiciona a classe 'not-organic' para estilização.
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-  }
-  // Retorna o template HTML com todos os marcadores de posição substituídos pelos dados do produto.
-  return output
-}
 
 /**
 * Cria um servidor HTTP.
